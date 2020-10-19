903ebe01ea48a1c556ce561b264a8e54bd22922a
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-10-19 14:30:19 +0200

    Added routes using react router dom, fixed sending data to firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-          <BurgerBuilder/>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Switch>
+                        <Route exact path={'/'} component={BurgerBuilder}/>
+                        <Route path={'/checkout'} component={Checkout}/>
+                    </Switch>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
