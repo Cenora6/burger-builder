1605a0c5ac47ca58b44420aac3e291579b1539d6
Cenora6 <aleksandra.gasidlo@gmail.com>
2020-11-02 16:24:38 +0100

    Fetching orders data with React Redux

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -29,7 +29,18 @@ const reducer = (state = initialState, action) => {
                 purchased: true,
                 orders: state.orders.concat(newOrder)
             };
-        case actionTypes.PURCHASE_BURGER_FAIL:
+        case actionTypes.FETCH_ORDERS_START:
+            return {
+                ...state,
+                loading: true
+            }
+        case actionTypes.FETCH_ORDERS_SUCCESS:
+            return {
+                ...state,
+                orders: action.orders,
+                loading: false
+            };
+        case actionTypes.FETCH_ORDERS_FAIL:
             return {
                 ...state,
                 loading: false
